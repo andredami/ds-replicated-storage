a0ed006dc872b5c728d380763e147e582ec1efad
andreadamiani90@gmail.com <andreadamiani90@gmail.com@4a13126b-9b31-1349-e25d-13a1d202e7bc>
2014-01-23 19:32:38 +0000

    Client, HistoryBuffer, Message, OrderedMessage, ServerRemoteInterface and StorageMap implemented and documented.

--- trunk/DSReplicatedStorage/src/repst/Message.java
+++ trunk/DSReplicatedStorage/src/repst/Message.java
@@ -6,25 +6,97 @@ package repst;
 import java.io.Serializable;
 
 /**
+ * Represents an immutable Member Message sent Point-To-Point to the {@link Sequencer}
+ * 
  * @author Andrea
- *
+ * 
  */
 public class Message {
-	
-	private static Long nextId = (long) 0;
-	
-	private Serializable object;
+
+	private static long nextId = (long) 0;
+
 	/**
-	 * Acquired via config phase.
+	 * The content of the message
 	 */
-	private Long processId;
+	public final Serializable payload;
 	/**
-	 * Auto-incremented from nextId
+	 * The process ID acquired during the replica configuration phase
 	 */
-	private Long messageId;
+	public final long processId;
+	/**
+	 * The message ID local to the replica. Do not confuse it with the sequence
+	 * number present in messages sent by the sequencer
+	 */
+	public final long messageId;
+
+	/**
+	 * Acknowledges the highest sequence number received by the replica (Piggybacking)
+	 */
+	public final long lastSequence;
+
+	/**
+	 * Builds the immutable Message
+	 * 
+	 * @param payload The content of the message
+	 * @param processId The process ID acquired during the replica configuration phase
+	 * @param lastSequence Acknowledges the highest sequence number received by the replica (Piggybacking)
+	 */
+	public Message(Serializable payload, long processId, long lastSequence) {
+		this.payload = payload;
+		this.processId = processId;
+		this.messageId = nextId++;
+		this.lastSequence = lastSequence;
+	}
 	
 	/**
-	 * Ack on Piggyback 
+	 * Rebuilds the immutable Message
+	 * 
+	 * @param message The message to be rebuilt
 	 */
-	private Long lastSequence;
+	public Message(Message message) {
+		this.payload = message.payload;
+		this.processId = message.processId;
+		this.messageId = message.messageId;
+		this.lastSequence = message.lastSequence;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (lastSequence ^ (lastSequence >>> 32));
+		result = prime * result + (int) (messageId ^ (messageId >>> 32));
+		result = prime * result + ((payload == null) ? 0 : payload.hashCode());
+		result = prime * result + (int) (processId ^ (processId >>> 32));
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Message other = (Message) obj;
+		if (lastSequence != other.lastSequence)
+			return false;
+		if (messageId != other.messageId)
+			return false;
+		if (payload == null) {
+			if (other.payload != null)
+				return false;
+		} else if (!payload.equals(other.payload))
+			return false;
+		if (processId != other.processId)
+			return false;
+		return true;
+	}
 }
--- trunk/DSReplicatedStorage/src/repst/OrderedMessage.java
+++ trunk/DSReplicatedStorage/src/repst/OrderedMessage.java
@@ -1,12 +1,83 @@
-/**
- * 
- */
 package repst;
 
 /**
+ * Represents a wrapper for an immutable {@link Message} to be forwarded to all
+ * replicas.
+ * 
  * @author Andrea
- *
  */
 public class OrderedMessage extends Message {
-	private Long sequence;
+	/**
+	 * The message sequence number
+	 */
+	public final long sequenceNumber;
+
+	/**
+	 * Wraps a {@link Message} in the immutable OrderedMessage to be delivered
+	 * to other replicas
+	 * 
+	 * @param sequenceNumber
+	 *            The message sequence number
+	 * @param message
+	 *            The message to be wrapped and forwarded
+	 */
+	public OrderedMessage(long sequenceNumber, Message message) {
+		super(message);
+		this.sequenceNumber = sequenceNumber;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result
+				+ (int) (sequenceNumber ^ (sequenceNumber >>> 32));
+		return result;
+	}
+
+	/**
+	 * Compares the two {@link OrderedMessage}s only with respect to their
+	 * sequence number. It can be used because the sequence number must be
+	 * unique for each {@link Message}
+	 * 
+	 * @param obj
+	 *            the reference {@link OrderedMessage} with which to compare.
+	 * @return {@code true} if this object is the same as the obj argument;
+	 *         {@link false} otherwise.
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	public boolean equalsSN(OrderedMessage obj) {
+		if (this == obj)
+			return true;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderedMessage other = (OrderedMessage) obj;
+		if (sequenceNumber != other.sequenceNumber)
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderedMessage other = (OrderedMessage) obj;
+		if (sequenceNumber != other.sequenceNumber)
+			return false;
+		return true;
+	}
 }
