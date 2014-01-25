4c6d07ec97baebe589f7682e35a79918818ee825
jacopo.aliprandi@gmail.com <jacopo.aliprandi@gmail.com@4a13126b-9b31-1349-e25d-13a1d202e7bc>
2014-01-25 16:48:26 +0000

    business logic sketch implemented in server and channel: rmi and socket to be added

--- trunk/DSReplicatedStorage/src/repst/Channel.java
+++ trunk/DSReplicatedStorage/src/repst/Channel.java
@@ -4,39 +4,136 @@
 package repst;
 
 import java.io.Serializable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author Jacopo
- *
+ * 
  */
 public class Channel {
-	
+
 	private Long lastSequence;
-	private LinkedList<Message> queue;
+	private Long mySequence;
 	private Long processId;
-	
-	public void initialize(){
-		// TODO remote call to sequencer getNewProcessId() and stores it in processId
+
+	private LinkedList<Message> deliveryQueue = new LinkedList<Message>();
+	private LinkedList<Message> holdBackQueue = new LinkedList<Message>();
+	private ExecutorService pool = Executors.newCachedThreadPool();
+
+	public void initialize() {
+		//TODO open and initialize the ip multicast socket 
+		// TODO remote call to sequencer getNewProcessId() and stores it in
+		// processId
+		pool.execute(readFromSocket);
 	}
+
 	
-	public void forward(Serializable obj){
-		//TODO creates Message and sends it to Sequencer
-	}
+
 	
-	private void sendNack(){
-		// TODO calls remote getLost... on Sequencer
-	}
-	
-	public void onMulticastReceived(OrderedMessage msg){
-		if(msg.sequence == lastSequence+1){
-			// TODO push in queue if not duplicated
+	private void onMulticastReceived(OrderedMessage msg) {
+		if (msg.sequenceNumber == lastSequence + 1) {
+			lastSequence=msg.sequenceNumber;
+			putInDeliveryQueue(msg);
 		} else {
-			// TODO discard, pause socket and sendNack()
+			discardAndSendNack();
 		}
 	}
-	
-	public Serializable pop(){
-		// TODO return the obj in the head message to the server and deletes it.
+
+	private Runnable readFromSocket = new Runnable() {
+
+		@Override
+		public void run() {
+			while (true) {
+				// TODO read from socket and call onMulticastReceived
+			}
+
+		}
+	};
+
+	private void discardAndSendNack() {
+		// TODO Auto-generated method stub
+
 	}
-	
+
+	/**
+	 * Performed in single thread readFromSocket. insert at the end of the
+	 * delivery queue. If the message is in the holdbackQueue the reference to
+	 * the payload is replaced.
+	 * 
+	 * @param msg
+	 */
+	private void putInDeliveryQueue(OrderedMessage msg) {
+		Message toBedelivered = msg;
+		if (msg.processId == processId) {
+			synchronized (holdBackQueue) {
+				Iterator<Message> i = holdBackQueue.iterator();
+				Message m = null;
+				while (i.hasNext()) {
+					m = i.next();
+					if (m.messageId == msg.messageId) {
+						toBedelivered = new OrderedMessage(msg.sequenceNumber,
+								m);// keep the same
+									// reference
+						i.remove();
+						break;
+					}
+				}
+			}
+		}
+
+		deliveryQueue.addLast(toBedelivered);
+		deliveryQueue.notifyAll();
+	}
+
+	/**
+	 * 
+	 * @return
+	 * @throws InterruptedException
+	 */
+	public Serializable pop() throws InterruptedException {
+		Serializable s = null;
+		synchronized (deliveryQueue) {
+			while (deliveryQueue.isEmpty()) {
+				deliveryQueue.wait();
+			}
+			Message m = deliveryQueue.poll();
+			s = m.payload;
+		}
+		return s;
+
+	}
+
+	/**
+	 * Asynchronous write operation: only performs the call to the sequencer.
+	 * Ensures that the same reference of payload is returned back when reading
+	 * from the channel: in this way the server is not aware of the group and we
+	 * do not have conflicts in tell apart my payload from the payload of
+	 * others.
+	 * 
+	 * @param payload
+	 */
+	public synchronized void write(final Payload payload) {
+
+		long assignedSeq;
+		synchronized (mySequence) {
+			assignedSeq = ++mySequence;
+		}
+		Message m = new Message(payload, processId, assignedSeq);
+		synchronized (holdBackQueue) {
+			holdBackQueue.addLast(m);
+		}
+		sendToSequencer(m);
+
+	}
+
+	private void sendToSequencer(Message m) {
+		// TODO Auto-generated method stub
+	}
+	private void sendNack() {
+		// TODO calls remote getLost... on Sequencer
+	}
+
 }
--- trunk/DSReplicatedStorage/src/repst/Server.java
+++ trunk/DSReplicatedStorage/src/repst/Server.java
@@ -3,38 +3,131 @@
  */
 package repst;
 
-import java.util.Set;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author Jacopo
- *
+ * 
  */
 public class Server implements ServerRemoteInterface {
 
-	private StorageMap storage;
+	private StorageMap storage = new StorageMap();
+	private Channel channel = new Channel();
+	private List<Payload> operations = new LinkedList<Payload>();
+	private ExecutorService pool = Executors.newCachedThreadPool();
 
 	// TODO main - createChannel calls initialize
-	
-	/* (non-Javadoc)
+	public static void main(String[] args) {
+
+		// take parameters?
+
+		// initialize server object
+		System.out.println("Initializing server...");
+		Server server = new Server();
+		server.initialize();
+
+		// register the rmi object
+
+	}
+
+	private void initialize() {
+		channel.initialize();
+		pool.execute(readloop);
+
+	}
+
+	private Runnable readloop = new Runnable() {
+
+		@Override
+		public void run() {
+			while (true) {
+				fetchFormChannel();
+			}
+
+		}
+	};
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see repst.ServerRemoteInterface#readValue(java.lang.Integer)
 	 */
 	@Override
 	public Integer readValue(Integer key) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return storage.getValue(key);
 	}
 
-	/* (non-Javadoc)
-	 * @see repst.ServerRemoteInterface#writeValue(java.lang.Integer, java.lang.Integer)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see repst.ServerRemoteInterface#writeValue(java.lang.Integer,
+	 * java.lang.Integer)
 	 */
 	@Override
-	public void writeValue(Integer key, Integer value) {
-		// TODO Calls forward
+	public synchronized void writeValue(Integer key, Integer value) {
+		Payload p = new Payload(key, value);
+		synchronized (operations) {
+			operations.add(operations.size(), p);
+		}
+
+		channel.write(p);// asynch call
+		try {
+			p.wait();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return;
 
 	}
-	
-	public void fetchFormChannel(){
-		// TODO pops from Channel and puts into storage.
+
+	/**
+	 * performed by a single thread in a while loop.(to ensure order between
+	 * delivered messages.
+	 */
+	public void fetchFormChannel() {
+
+		Payload p;
+		try {
+			p = (Payload) channel.pop();
+			performAndnotifyWriteOp(p);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * Remove from the pending operation list and notify the thread who is
+	 * waiting for that operation.
+	 * 
+	 * @param pNew
+	 *            the paylod delivered by channel.
+	 */
+	private void performAndnotifyWriteOp(Payload pNew) {
+		Payload pOld = null;
+		boolean my = false;
+		synchronized (operations) {
+			Iterator<Payload> i = operations.iterator();
+			while (i.hasNext()) {
+				pOld = i.next();
+				if (pOld == pNew) {
+					i.remove();
+					my = true;
+					break;
+				}
+			}
+		} // release lock on operations
+
+		// this write is performed in a single thread: no worries about ordering
+		storage.putValue(pNew.key, pNew.value);
+		if (pOld != null && my == true) {
+			pOld.notify();
+		}
 	}
 
 }
