fca60c26307826daaaf2d4ebfcff91e0a3a4df75
jacopo.aliprandi@gmail.com <jacopo.aliprandi@gmail.com@4a13126b-9b31-1349-e25d-13a1d202e7bc>
2014-01-31 23:37:39 +0000

    lamport riletto e aggiustato

--- DSReplicatedStoragev2/src/repst/LamportAck.java
+++ DSReplicatedStoragev2/src/repst/LamportAck.java
@@ -2,6 +2,11 @@ package repst;
 
 public class LamportAck extends LMessage{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 722359936312486841L;
+
 	public LamportAck(long processId, long lamportClock) {
 		super(processId, lamportClock);
 	}
--- DSReplicatedStoragev2/src/repst/LamportMessage.java
+++ DSReplicatedStoragev2/src/repst/LamportMessage.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 public class LamportMessage extends LMessage {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4595885519563413434L;
+	
 	public LamportMessage(Payload payload, int processId, long assignedClock) {
 		super(processId, assignedClock);
 		this.payload=payload;
